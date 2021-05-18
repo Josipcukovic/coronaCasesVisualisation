9cfd8a2bd2b3424c0257061d0c4de906e43a3887
Josipcukovic <cukone1@gmail.com>
2021-05-18 23:36:46 +0200

    added pie chart

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var width = 960;
+var width = 670;
 var height = 700;
 
-const dims = { height: 300, width: 300, radius: 150 };
-const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5) };
+const dims = { height: 300, width: 300, radius: 75 };
+//const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5) };
+
 
 var projection = d3.geoMercator()
     .center([0, 10])
@@ -18,15 +19,30 @@ var svg = d3.select(".canvas").append("svg")
     .attr("height", height)
     .style("background", "white");
 
-var groupMap = svg.append("g").attr("width", width).attr("height", height);
-var groupPieChart = svg.append("g").attr("width", dims.width + 150).attr("height", dims.height + 150).attr("transform", "translate(600,250)");
-const pie = d3.pie().sort(null).value(d => d.broj_zarazenih);
+var pieChartSvg = d3.select(".canvas").append("svg")
+    .attr("width", 350)
+    .attr("height", 350)
+    .attr("transform", "translate(0,-300)");
 
-const angles = pie([
-    { "Male": "kk", "broj_zarazenih": 150 },
-    { "Male": "kksasa", "broj_zarazenih": 50 },
-    { "Male": "kksasa", "broj_zarazenih": 100 }
-]);
+var groupMap = svg.append("g").attr("width", width).attr("height", height);
+//piechart
+var groupPieChart = pieChartSvg.append("g").attr("width", dims.width).attr("height", dims.height).attr("transform", "translate(75,75)");
+var groupPieChartLegend = pieChartSvg.append("g").attr("transform", `translate(160, 30)`);
+
+const colorsPieChart = d3.scaleOrdinal(['#72bcd4', '#ff3232']);
+
+const legend = d3.legendColor()
+    .shape("circle")
+    .scale(colorsPieChart)
+    .title("Zaraženi prema spolu");
+
+const pie = d3.pie().sort(null).value(d => d.value);
+
+const arcPath = d3.arc().outerRadius(dims.radius).innerRadius(dims.radius / 2);
+
+
+//pie
+
 
 
 const tip = d3.tip()
@@ -34,12 +50,67 @@ const tip = d3.tip()
 
 groupMap.call(tip);
 
-const arcPath = d3.arc().outerRadius(dims.radius).innerRadius(dims.radius / 2);
+const update = (data, i) => {
+    let pieData = [];
+    pieData = getDataByGender(data, i);
+    //domain
+    colorsPieChart.domain(pieData.map(d => d.id));
+    console.log(pieData);
+    groupPieChartLegend.call(legend);
 
-console.log(arcPath(angles[0]));
 
 
+    //setting paths
+    const paths = groupPieChart.selectAll("path")
+        .data(pie(pieData));
+    //deleting elements
+    paths.exit().remove();
+    ///update current elements
+    paths.attr("d", arcPath)
+        .transition()
+        .duration(750)
+        .attrTween("d", arcTweenUpdate);
+    ///create elements for data provided
+    paths.enter()
+        .append("path")
+        .attr("class", "arc")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 3)
+        .attr("fill", d => colorsPieChart(d.data.id))
+        .each(function (d) { this.trenutno = d })
+        .transition()
+        .duration(750)
+        .attrTween("d", arcTweenEnter);
 
+    //console.log(podaci);
+    // console.log(ukupno);
+    // console.log((musko / ukupno) * 100);
+    // console.log((zensko / ukupno) * 100);
+
+};
+
+function getDataByGender(data, i) {
+    const newData = []
+
+    var male = 0;
+    var female = 0;
+
+    data.forEach(element => {
+
+        if (element.spol == "M" && element.Zupanija == i.properties.name) {
+            male++;
+        } else if (element.spol == "Ž" && element.Zupanija == i.properties.name) {
+            female++;
+        }
+
+    });
+
+    var total = male + female;
+    newData.push({ "id": "Muškarci", "value": (male / total) * 100 });
+    newData.push({ "id": "Žene", "value": (female / total) * 100 });
+
+    return newData;
+};
 
 d3.json("cro_regv3.json").then((cro) => {
     var data = topojson.feature(cro, cro.objects.layer1);
@@ -69,22 +140,8 @@ d3.json("cro_regv3.json").then((cro) => {
         .style("stroke-opacity", 1)
         .on("click", (d, i) => {
 
-
             d3.json("poOsobama.json").then((data) => {
-                var musko = 0;
-                var zensko = 0;
-                console.log(data);
-                data.forEach(element => {
-                    if (element.spol == "M" && element.Zupanija == i.properties.name) {
-                        musko++;
-                    } else if (element.spol == "Ž" && element.Zupanija == i.properties.name) {
-                        zensko++;
-                    }
-                });
-                var ukupno = musko + zensko;
-                console.log(ukupno);
-                console.log((musko / ukupno) * 100);
-                console.log((zensko / ukupno) * 100);
+                update(data, i);
             });
         })
         .on("mouseover", (d, i, n) => {
@@ -107,18 +164,39 @@ function handleHover(d) {
         return `${d.properties.name} <br> Broj zaraženih: ${d.properties.broj_zarazenih} <br> Broj umrlih: ${d.properties.broj_umrlih} `
     });
     tip.show(d, event.target);
-}
+};
 
 function handleHoverOut(d) {
     tip.hide()
     d3.select(event.target).style("opacity", 1);
+};
+// groupPieChart.append('circle')
+//     .attr('cx', 100)
+//     .attr('cy', 100)
+//     .attr('r', 50)
+//     .attr('stroke', 'black')
+//     .attr('fill', '#69a3b2')
+//     .on("mouseover", (d) => {
+//         console.log(event);
+//     });
+
+const arcTweenEnter = (d) => {
+    var i = d3.interpolate(d.endAngle, d.startAngle);
+
+    return function (t) {
+        d.startAngle = i(t);
+        return arcPath(d);
+    }
+};
+
+function arcTweenUpdate(d) {
+
+    console.log(this.trenutno, d);
+    var i = d3.interpolate(this.trenutno, d);
+
+    this.trenutno = d;
+
+    return function (t) {
+        return arcPath(i(t));
+    }
 }
-groupPieChart.append('circle')
-    .attr('cx', 100)
-    .attr('cy', 100)
-    .attr('r', 50)
-    .attr('stroke', 'black')
-    .attr('fill', '#69a3b2')
-    .on("mouseover", (d) => {
-        console.log(event);
-    });
